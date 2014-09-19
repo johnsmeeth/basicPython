195ddb6a493181bfe9510aa8d59a8ffeda9b6a2e
JohnSmeeth <johnsmeeth0210@gmail.com>
2014-09-20 00:12:22 +0700

    print with many awsome

---
+++ print.py
@@ -0,0 +1,17 @@
+## Use print to print string
+print "Hello " * 5
+print "This is first line",
+print "this is second line but still stand the same line with first line"
+
+# Using """ to print origin format of text like pre tag on HTML
+print """
+    Tao the day la dong 1
+    tao the day la dong 2
+        va tao cung the dong nay la dong 3 va thut vao
+    """
+# Draw a rectangula
+line1 = "." * 41
+line2 = "." + "\t" * 5 + "." + "\n"
+print line1
+print line2 * 6,
+print line1
