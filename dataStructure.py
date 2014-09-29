24491344d8556a2510d4043a77ef0229679bb4ce
JohnSmeeth <johnsmeeth0210@gmail.com>
2014-09-29 22:54:25 +0700

    io python awsome

---
+++ dataStructure.py
@@ -0,0 +1,6 @@
+__author__ = 'thanhnguyen'
+## list: this data structure like array on some language such as c/c++, javascript...
+cars = ["BMW", "Audi", 'Mercedes', 88, 99, 'Bentley'] # Create a new list
+
+print cars[0:2]
+print cars[:-1]
--- list.py
+++ list.py
@@ -9,4 +9,32 @@ print "--------------"
 
 n = 0
 while n < len(cars):
-    print cars[n]
+    print cars[n], len(cars[n])
+    n += 1
+
+
+print "Using range method to create an array"
+for i in range(10):
+    print i,
+
+print '\nlist created from start and end use range'
+for i in range(4, 9):
+    print i,
+
+print '\nlist created from start, end, step use range'
+for i in range(0, 10, 2):
+    print i,
+
+print '\n Combine range, len and for'
+for i in range(len(cars)):
+    print i, cars[i]
+
+print '\nExercise GIAITHUA'
+def gt(n):
+    if n <= 1:
+        return 1
+    else:
+        return n * gt(n-1)
+
+n = int(raw_input('Enter n: '))
+print n, "! = ", gt(n)
